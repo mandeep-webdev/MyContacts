914ce6f5e4cf55f3a6b406683e9e319c6725f8de
Inderjit Singh <mandeep.fullstack.dev@gmail.com>
2024-12-19 13:40:33 +0530

    fetch the data from firestore database

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,31 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import Header from "./components/Header";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./config/firebase";
 const App = () => {
+  const [contacts, setContacts] = useState([]);
+  useEffect(() => {
+    const fetchContact = async () => {
+      try {
+        const collectionRef = collection(db, "contacts");
+        // querySnapshot -- This is an array of objects
+        const querySnapshot = await getDocs(collectionRef);
+        const data = querySnapshot.docs.map((doc) => {
+          return {
+            id: Math.random(),
+            ...doc.data(),
+          };
+        });
+
+        setContacts(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchContact();
+  }, []);
   return (
     <div className="max-w-[370px] mx-auto">
       <Header />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,24 @@
 import React from "react";
-
+import { IoIosContacts } from "react-icons/io";
 import { IoSearch } from "react-icons/io5";
 import { FaPlusCircle } from "react-icons/fa";
 const Header = () => {
   return (
     <div className="flex gap-2 h-[100px] justify-center">
-      <div className="flex flex-grow relative items-center">
+      <div className="flex items-center ">
+        <IoIosContacts className="text-white text-4xl" />
+      </div>
+      <div className="flex relative items-center">
         <IoSearch className=" ml-1 text-white text-2xl absolute" />
         <input
           type="text"
           placeholder="Search Contact"
-          className=" pl-8 text-white bg-transparent  border border-white rounded-md h-9 flex-grow px-4 text-sm"
+          className=" focus:outline-none pl-8 text-white bg-transparent  border border-white rounded-md h-9 flex-grow px-4 text-sm"
         />
       </div>
 
       <button>
-        <FaPlusCircle className=" text-4xl text-white" />
+        <FaPlusCircle className=" text-4xl text-white cursor-pointer " />
       </button>
     </div>
   );
