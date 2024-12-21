de97a2b55bdffe57d15b031804f99c737dd1c4ee
Inderjit Singh <mandeep.fullstack.dev@gmail.com>
2024-12-21 13:50:53 +0530

    make changes in modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,51 +1,52 @@
 import React from "react";
 import { IoMdClose } from "react-icons/io";
 const Modal = ({ closeModal }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center ">
-      <div className="bg-white p-6 rounded-lg w-96">
-        <h2 className="text-xl font-semibold mb-4">Add New Contact</h2>
-        <form>
+    <div className="fixed inset-0 flex justify-center items-center backdrop-blur-sm">
+      <div className=" bg-white p-6 rounded-lg w-96">
+        <h2 className="text-xl font-semibold mb-2 text-slate-600 text-center">
+          Add New Contact
+        </h2>
+        <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label
               htmlFor="name"
-              className="block text-sm font-medium text-gray-700"
+              className="block text-sm font-medium text-slate-600 mb-1"
             >
               Name
             </label>
             <input
               type="text"
               id="name"
-              className="mt-1 p-2 w-full border border-gray-300 rounded-md"
               placeholder="Enter name"
+              className="border w-full border-slate-800 p-2 rounded-md outline-none text-slate-600"
             />
           </div>
           <div className="mb-4">
             <label
               htmlFor="mobile"
-              className="block text-sm font-medium text-gray-700"
+              className="block text-sm font-medium text-slate-600 mb-1"
             >
-              Mobile Number
+              Mobile
             </label>
             <input
               type="text"
               id="mobile"
-              className="mt-1 p-2 w-full border border-gray-300 rounded-md"
               placeholder="Enter mobile number"
+              className="border w-full border-slate-800 p-2 rounded-md outline-none text-slate-600"
             />
           </div>
-          <div className="flex justify-end gap-2">
+          <div className="flex justify-end gap-2 py-2">
             <button
-              type="button"
-              className="px-4 py-2 bg-gray-300 text-black rounded-md"
+              className="border border-transparent px-2 rounded-md cursor-pointer hover:text-red-700 transition duration-300"
               onClick={closeModal}
             >
-              Close
+              Cancel
             </button>
-            <button
-              type="submit"
-              className="px-4 py-2 bg-blue-600 text-white rounded-md"
-            >
+            <button className=" border bg-blue-500 hover:bg-blue-600 transition duration-300 text-white p-2 rounded-md cursor-pointer">
               Add Contact
             </button>
           </div>
